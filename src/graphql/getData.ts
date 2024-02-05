86760fc1102a38d56617162fd8f1162e2617917e
Agata <kosekboom@gmail.com>
2024-02-05 20:40:19 +0100

    graphql refacotr

--- src/graphql/getData.ts
+++ src/graphql/getData.ts
@@ -1,55 +1,56 @@
 import { gql } from "@apollo/client";
 
+// FRAGMENTS
+const CHARACTER_FIELDS = gql`
+  fragment CharacterFields on Character {
+    id
+    name
+    image
+    status
+    species
+    type
+    gender
+    location {
+      name
+    }
+    origin {
+      id
+      name
+    }
+    episode {
+      id
+      episode
+      name
+    }
+  }
+`;
+
+// QUERIES
 export const GET_ALL_CHARACTERS = gql`
+  ${CHARACTER_FIELDS}
   query getAllCharacters($page: Int) {
     characters(page: $page) {
       info {
         pages
       }
       results {
-        id
-        name
-        image
-        status
-        species
-        origin {
-          name
-        }
-        episode {
-          id
-          episode
-          name
-        }
+        ...CharacterFields
       }
     }
   }
 `;
 
 export const GET_SINGLE_DATA = gql`
+  ${CHARACTER_FIELDS}
   query fetchCharacters($id: ID!) {
     character(id: $id) {
-      id
-      name
-      image
-      status
-      species
-      type
-      gender
-      location {
-        name
-      }
-      origin {
-        name
-      }
-      episode {
-        name
-        episode
-      }
+      ...CharacterFields
     }
   }
 `;
 
 export const FILTER_BY_NAME = gql`
+  ${CHARACTER_FIELDS}
   query getCharacterByName($page: Int, $name: String!) {
     characters(filter: { name: $name }, page: $page) {
       info {
@@ -57,28 +58,14 @@ export const FILTER_BY_NAME = gql`
         count
       }
       results {
-        id
-        name
-        image
-        status
-        species
-        location {
-          name
-        }
-        origin {
-          id
-          name
-        }
-        episode {
-          id
-          name
-        }
+        ...CharacterFields
       }
     }
   }
 `;
 
 export const FILTER_BY_STATUS = gql`
+  ${CHARACTER_FIELDS}
   query getCharacterByStatus($page: Int, $status: String!) {
     characters(filter: { status: $status }, page: $page) {
       info {
@@ -86,22 +73,7 @@ export const FILTER_BY_STATUS = gql`
         count
       }
       results {
-        id
-        name
-        image
-        status
-        species
-        location {
-          name
-        }
-        origin {
-          id
-          name
-        }
-        episode {
-          id
-          name
-        }
+        ...CharacterFields
       }
     }
   }
